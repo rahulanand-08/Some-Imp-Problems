65634090a9a46362da63edf6085f3d3801662945
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-07-14 21:34:15 +0530

    Update RemoveDuplicatesFromUnsortedList.java

--- RemoveDuplicatesFromUnsortedList.java
+++ RemoveDuplicatesFromUnsortedList.java
@@ -1,1 +1,18 @@
+//https://www.codingninjas.com/studio/problems/remove-duplicates-from-unsorted-linked-list_1069331?source=youtube&campaign=YouTube_Lovebabbar29thJan&leftPanelTab=3
 
+public static LinkedListNode<Integer> removeDuplicates(LinkedListNode<Integer> head) {
+    HashSet<Integer> set = new HashSet<>();
+    LinkedListNode<Integer> current = head;
+    LinkedListNode<Integer> prev = null;
+
+    while (current != null) {
+      if (set.contains(current.data)) {
+        prev.next = current.next;
+      } else {
+        set.add(current.data);
+        prev = current;
+      }
+      current = current.next;
+    }
+    return head;
+  }
