14070e2dafe161d7d07e85a14adc70ac8f25a10e
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-07-14 21:37:56 +0530

    Update MergeTwoSortedList.java

--- MergeTwoSortedList.java
+++ MergeTwoSortedList.java
@@ -1,1 +1,23 @@
+//https://leetcode.com/problems/merge-two-sorted-lists/
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head=new ListNode(0);
+        ListNode curr=head;
+        while (list1!=null && list2!=null)
+        {
+            if (list1.val<=list2.val)
+            {
+                curr.next=list1;
+                list1=list1.next;
+            }
+            else 
+            {
+                curr.next=list2;
+                list2=list2.next;
+            }
+            curr=curr.next;
+        }
+        return head.next;
+    }
 
